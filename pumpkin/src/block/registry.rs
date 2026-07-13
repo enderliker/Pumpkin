c906c2ec0145541269889336ee50503ba84c0bbe
enderliker <enderliker@users.noreply.github.com>
2026-07-13 17:37:43 -0300

    style: resolve clippy semicolon warnings in block/world event emission

--- pumpkin/src/block/registry.rs
+++ pumpkin/src/block/registry.rs
@@ -632,18 +632,16 @@ impl BlockRegistry {
         .await;
 
         // Vibration: block place
-        {
-            use crate::world::game_event::VibrationSource;
-            use pumpkin_data::game_event::GameEvent;
-            let source = if player.get_entity().is_sneaking() {
-                VibrationSource::PLAYER_SNEAKING
-            } else {
-                VibrationSource::PLAYER
-            };
-            world
-                .emit_game_event(final_block_pos.to_centered_f64(), GameEvent::BlockPlace, source)
-                .await
+        use crate::world::game_event::VibrationSource;
+        use pumpkin_data::game_event::GameEvent;
+        let source = if player.get_entity().is_sneaking() {
+            VibrationSource::PLAYER_SNEAKING
+        } else {
+            VibrationSource::PLAYER
         };
+        world
+            .emit_game_event(final_block_pos.to_centered_f64(), GameEvent::BlockPlace, source)
+            .await;
 
         Ok(Some((final_block_pos, new_state)))
     }
--- pumpkin/src/world/mod.rs
+++ pumpkin/src/world/mod.rs
@@ -4536,19 +4536,17 @@ impl World {
             let broken_state_id = self.set_block_state(position, new_state_id, flags).await;
 
             // Vibration: block destroy (player or other)
-            {
-                use crate::world::game_event::VibrationSource;
-                use pumpkin_data::game_event::GameEvent;
-                let source = cause.as_ref().map_or(VibrationSource::NONE, |player| {
-                    if player.get_entity().is_sneaking() {
-                        VibrationSource::PLAYER_SNEAKING
-                    } else {
-                        VibrationSource::PLAYER
-                    }
-                });
-                self.emit_game_event(position.to_centered_f64(), GameEvent::BlockDestroy, source)
-                    .await
-            };
+            use crate::world::game_event::VibrationSource;
+            use pumpkin_data::game_event::GameEvent;
+            let source = cause.as_ref().map_or(VibrationSource::NONE, |player| {
+                if player.get_entity().is_sneaking() {
+                    VibrationSource::PLAYER_SNEAKING
+                } else {
+                    VibrationSource::PLAYER
+                }
+            });
+            self.emit_game_event(position.to_centered_f64(), GameEvent::BlockDestroy, source)
+                .await;
 
             // Close container screens for any players viewing this block
             self.close_container_screens_at(position).await;
